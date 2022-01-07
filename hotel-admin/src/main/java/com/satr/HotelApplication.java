8d03dd03d1acdec73578767646221178ad50177d
zut-cs-427 <859193125@qq.com>
2022-01-07 20:32:46 +0800

    创建项目

---
+++ hotel-admin/src/main/java/com/satr/HotelApplication.java
@@ -0,0 +1,17 @@
+package com.satr;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动程序
+ *
+ * @author  聂建强
+ */
+@SpringBootApplication
+public class HotelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HotelApplication.class, args);
+    }
+}
